0213461c7046d1a1262b0eee710e0851901b3c29
MrRad1st <Radin1383rvn@gmail.com>
2025-02-22 23:29:02 +0330

    added the dataset, global model and ...
    fitting the models of devices that are represented by 1 in the bitstring
    added current_learning_iteration to the whole code
    added self.number_of_times_fitted to each device
    added dataset and random data division between devices and 20 percent of the data will be set as the test data for the global model

--- version 5/2.py
+++ version 5/2.py
@@ -5,6 +5,9 @@ from tensorflow import keras
 from keras import layers
 # from sklearn.model_selection import train_test_split
 
+
+current_learning_iteration = 0
+
 # Device Class
 class Device:
     def __init__(self, device_id, ram, storage, cpu, bandwidth, battery, charging):
@@ -18,6 +21,8 @@ class Device:
         self.energy_consumption = ram + storage + cpu + bandwidth
         self.model = self.create_model()
         self.data = None  # Placeholder for dataset partition
+        
+        self.number_of_times_fitted = 0
 
     def create_model(self):
         model = keras.Sequential([
@@ -47,4 +52,66 @@ for _, row in df.iterrows():
     )
     devices.append(device)
 
+
+
+
+
+
+# Load MNIST dataset
+(x_train, y_train), (_, _) = keras.datasets.mnist.load_data()
+
+# Normalize data and reshape for CNN
+x_train = x_train.astype("float32") / 255.0
+x_train = np.expand_dims(x_train, -1)  # Add channel dimension
+
+# Shuffle data
+indices = np.arange(len(x_train))
+np.random.shuffle(indices)
+x_train, y_train = x_train[indices], y_train[indices]
+
+# Split into global test set (20%) and training set (80%)
+split_index = int(0.8 * len(x_train))
+x_train_devices, y_train_devices = x_train[:split_index], y_train[:split_index]
+x_test_global, y_test_global = x_train[split_index:], y_train[split_index:]
+
+
+# Split dataset among devices
+num_devices = len(devices)
+split_size = len(x_train_devices) // num_devices
+
+for i, device in enumerate(devices):
+    start = i * split_size
+    end = (i + 1) * split_size if i < num_devices - 1 else len(x_train_devices)
+    device.data = (x_train_devices[start:end], y_train_devices[start:end])
+
+
+
+
+
+with open('version 5/data/bitstring.txt', 'r') as f:
+    bitstring = f.read()
+
+bitstring = [int(bit) for bit in bitstring.split(',')]
+
+current_learning_iteration += 1
+for device in devices:
+    if bitstring[device.device_id] == 1:
+        device.model.fit(device.data[0], device.data[1], epochs=5, verbose=1)
+        device.number_of_times_fitted += 1
+
+
 # Global Model
+# Define the global model with the same architecture
+def create_global_model():
+    model = keras.Sequential([
+        layers.Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(28, 28, 1)),
+        layers.MaxPooling2D(pool_size=(2, 2)),
+        layers.Flatten(),
+        layers.Dense(128, activation='relu'),
+        layers.Dense(10, activation='softmax')
+    ])
+    model.compile(optimizer=keras.optimizers.SGD(learning_rate=0.01), 
+                  loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    return model
+
+global_model = create_global_model()
